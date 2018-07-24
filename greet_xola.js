2b81b2d1f30e5d9836a4b123cabe147d5a31c585
unalo <unalomtshokotsha@gmail.com>
2018-07-24 14:54:44 +0200

    used figlet

--- greet_xola.js
+++ greet_xola.js
@@ -1,6 +1,17 @@
-// const greet = require('./greet');
+const greet = require('./greet');
 // console.log(greet("xola"));
 
 const figlet = require('figlet');
-figlet("Hello xola")
-console.log();
+
+figlet.text('Xola!', {
+    font: 'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+}, function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
