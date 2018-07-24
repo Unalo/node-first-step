3dc84774def5468ac63f2db124584bc9d69bb72d
unalo <unalomtshokotsha@gmail.com>
2018-07-24 15:06:03 +0200

    used both

--- greet_xola.js
+++ greet_xola.js
@@ -1,17 +1,26 @@
-const greet = require('./greet');
+//const greet = require('./greet');
 // console.log(greet("xola"));
+
+
+const chalk = require('chalk');
+//import the greet module that is in the current folder
+const greet = require('./greet');
+
+const styledMessage = chalk.bgGreen.black(greet('Xola'));
+console.log(styledMessage)
+
 
 const figlet = require('figlet');
 
 figlet.text('Xola!', {
-    font: 'Ghost',
-    horizontalLayout: 'default',
-    verticalLayout: 'default'
+  font: 'Ghost',
+  horizontalLayout: 'default',
+  verticalLayout: 'default'
 }, function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    console.log(data);
+  if (err) {
+    console.log('Something went wrong...');
+    console.dir(err);
+    return;
+  }
+  console.log(data);
 });
